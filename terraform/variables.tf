bd68afa103431ef8f83b039ab4bd9b6537cefc93
Vijay Pandian <ananth.vijay@gmail.com>
2024-03-04 11:15:21 -0500

    Update variables.tf - add more variable definitions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,3 +16,23 @@ variable "object_storage_bucket" {
   description = "S3 bucket for server 4.x environment"
   default     = "cci4-bucket-test"
 }
+
+variable "cluster_oidc_provider_arn" {
+   description = "Cluster OIDC provider ARN for the EKS cluster"
+}
+
+variable "cluster_oidc_issuer_url" {
+   description = "Cluster OIDC Issuer URL"
+}
+
+variable "k8s_namespace" {
+   description = "CircleCI Server 4.x Namespace"
+}
+variable "aws_account_id" {
+  description = "AWS Account ID"
+}
+
+variable "object_storage_irsa_role_arn" {
+   description = "S3 object storage role ARN"
+}
+
